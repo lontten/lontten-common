d6e88e6f3ecccf4a7a67860a9775563ec87832b0
lontten <lontten@163.com>
2024-11-12 23:33:09 +0800

    ✨ 常用时间格式化

--- src/main/java/com/lontten/common/util/LnTimeUtil.java
+++ src/main/java/com/lontten/common/util/LnTimeUtil.java
@@ -18,13 +18,23 @@
  * Contributors    :  xxxx   <xx@xx.com>
  *                 |  yyyy   <yy@yy.com>
  * Created On      : <2024-10-18>
- * Last Modified   : <2024-10-18>
+ * Last Modified   : <2024-11-12>
  *
  * lontten-common: Lontten 项目使用的通用组件库
  * ------------------------------------------------------------*/
 package com.lontten.common.util;
 
+import java.time.format.DateTimeFormatter;
+
 public class LnTimeUtil {
+    public static String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+    public static String dateFormat = "yyyy-MM-dd";
+    public static String timeFormat = "HH:mm:ss";
+
+    public static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(dateTimeFormat);
+    public static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(dateFormat);
+    public static DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern(timeFormat);
+
 
     public static void sleep(long millis) {
         try {
